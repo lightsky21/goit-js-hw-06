b75c67a29c84e14d6a6479c91deb7cd809f8879b
Mariia Telehonenko <telegonenko1@ukr.net>
2022-04-02 10:02:14 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsListEl = document.querySelector('#ingredients');
+
+function makeIngredients(ingredients) {
+  return ingredients.map(ingredient => {
+    const ingredientEl = document.createElement('li');
+    ingredientEl.textContent = ingredient;
+    ingredientEl.classList.add('item');
+    return ingredientEl;
+  } );
+}
+
+const elements = makeIngredients(ingredients);
+
+ingredientsListEl.prepend(...elements);
